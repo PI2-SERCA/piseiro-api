139451fed08efa069adcbb44a34c83c72e5d3a8e
João Luis Baraky <jlbaraky@gmail.com>
2022-08-06 00:59:52 -0300

    Update /scribe to suport single cuts.

--- src/resources/scribe.py
+++ src/resources/scribe.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from flask_restful import Resource
-from shapely.geometry import box
+from shapely.geometry import box, Polygon
 from src.resources.utils import get_optimized_cut, get_scribe_lines
 import json
 import requests
@@ -10,7 +10,7 @@ class Scribe(Resource):
     def post(self):
         data = request.get_json(force=True)
 
-        for arg in ['cuts', 'repetitions', 'ceramic_data']:
+        for arg in ['cuts', 'repetitions', 'ceramic_data', 'type']:
         	if arg not in data.keys():
         		return jsonify(
         			error=f"Bad Request: Missing argument '{arg}'",
@@ -19,23 +19,40 @@ class Scribe(Resource):
         cuts = data['cuts']
         repetitions = data['repetitions']
         ceramic_data = data['ceramic_data']
+        cut_type = data['type']
         
-        try:
-            repetitions = int(repetitions)
-        except:
+        must = {'height', 'width'}
+        if must.intersection(set(ceramic_data.keys())) != must:
             return jsonify(
-                error="Bad Request: Wrong format in 'repetitions'",
+                error="Bad Request: Argument 'ceramic_data' invalid",
+                status=requests.codes.bad_request
+            )
+
+        if type(cut_type) is not int:
+            return jsonify(
+                error="Bad Request: Argument 'type' is not integer",
+                status=requests.codes.bad_request
+            )
+
+        if type(repetitions) is not int:
+            return jsonify(
+                error="Bad Request: Argument 'repetitions' is not integer",
                 status=requests.codes.bad_request
             )
 
         ceramic = box(0, 0, ceramic_data['width'], ceramic_data['height']) 
 
         result = []
-        for cut, reps in cuts:
-        	optimized_cut = get_optimized_cut(cut, ceramic) 
-        	scribe_lines = get_scribe_lines(optimized_cut, ceramic)
-        	#gcode = get_gcode(scribe_lines)
-        	result.append({'cut': [list(optimized_cut.exterior.coords), reps]})
+        for cut in cuts:
+            if cut_type:
+                optimized_cut = get_optimized_cut(cut["points"], ceramic) 
+                scribe_lines = get_scribe_lines(optimized_cut, ceramic)
+            else:
+                optimized_cut = Polygon(cut["points"])
+                scribe_lines = optimized_cut
+
+            #gcode = get_gcode(scribe_lines)
+            result.append({'points': list(optimized_cut.exterior.coords), "quantity": cut["quantity"]})
 
 
         return jsonify(

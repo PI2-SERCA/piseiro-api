4fade3fc62d94e50022733ba61040937e4920975
igor-paiva <igorbatistapaiva@outlook.com>
2022-08-30 02:25:41 -0300

    Removes duplicated to_json method from Room and Cut

--- src/model/cut.py
+++ src/model/cut.py
@@ -2,10 +2,4 @@ from src.model.cast import Cast
 
 
 class Cut(Cast):
-    def to_json(self):
-        return {
-            "name": self.name,
-            "points": self.points,
-            "defaults": self.defaults,
-            "segments": self.segments,
-        }
+    pass

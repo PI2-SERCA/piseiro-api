99a1a3f6ac9af14d38b6142ff06f9b8e9f09e24a
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2022-09-12 22:49:35 -0300

    Getting database url from envs

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 import os
 
 MONGO_SETTINGS = {
-    "host": "piseiro-db",
-    "port": 27017,
+    "host": os.getenv("DB_HOST", "piseiro-db"),
+    "port": os.getenv("DB_PORT", "27017"),
     "db": os.getenv("DB_NAME", "piseiro"),
     "username": os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASSWORD", "example"),

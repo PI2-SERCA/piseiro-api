1a2d076dd8fb1ba0e3fabcb5d791dd165ef70d8d
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2022-09-12 23:19:50 -0300

    Fix mongo port as int

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 
 MONGO_SETTINGS = {
     "host": os.getenv("DB_HOST", "piseiro-db"),
-    "port": os.getenv("DB_PORT", "27017"),
+    "port": int(os.getenv("DB_PORT", "27017")),
     "db": os.getenv("DB_NAME", "piseiro"),
     "username": os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASSWORD", "example"),
